7f4fdfb5b0054182b0d73bd7b462e2e41b07f62e
ANTIBIOTICS <abx@abx.pe.kr>
2024-04-16 08:34:28 +0900

    Autoloader 개선

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -1,25 +1,47 @@
 <?php
 
 namespace Room9Stone\YouTubeDownloader\Api;
+use RuntimeException;
 use function spl_autoload_register;
-use function sprintf, substr, strlen, str_replace;
+use function strlen, strncmp, substr, str_replace, sprintf;
 use function is_readable;
 use const DIRECTORY_SEPARATOR;
 
 class Autoloader {
 
-  public static function register(): bool {
-    return spl_autoload_register(function(String $class): void {
+  protected const NAMESPACE_PREFIX    = __NAMESPACE__;
+  protected const BASE_DIRECTORY      = __DIR__;
+  protected const DIRECTORY_SEPARATOR = DIRECTORY_SEPARATOR;
 
-      $namespacePrefix = sprintf("%s\\", __NAMESPACE__);
-      $classRelativePath = str_replace("\\", "/", substr($class, strlen($namespacePrefix)));
-      $classFilePath = sprintf("%s%s%s.php", __DIR__, DIRECTORY_SEPARATOR, $classRelativePath);
-
-      if (is_readable($classFilePath)) {
-        require_once($classFilePath);
-      }
-
-    });
+  public static function register(): void {
+    if (!spl_autoload_register([ self::class, "loadClass" ])) {
+      throw new RuntimeException("Autoloader registration failed.");
+    }
   }
 
-};
+  protected static function loadClass(string $class): void {
+
+    $namespacePrefix = self::NAMESPACE_PREFIX;
+    $namespaceLength = strlen($namespacePrefix);
+
+    if (strncmp($namespacePrefix, $class, $namespaceLength) !== 0) {
+      return;
+    }
+
+    $relativeClass = substr($class, $namespaceLength);
+    $classFilePath = sprintf("%s%s.php",
+      self::BASE_DIRECTORY,
+      str_replace("\\", self::DIRECTORY_SEPARATOR, $relativeClass)
+    );
+
+    if (!is_readable($classFilePath)) {
+      throw new RuntimeException("File not found or not readable at %s.", $classFilePath);
+    }
+
+    require_once($classFilePath);
+
+  }
+
+  private function __construct() {}
+
+}
